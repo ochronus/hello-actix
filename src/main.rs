d6d9f5d51109231b78d6e743c7b1ddc6cef07944
Csaba Okrona <ochronus@ochronus.com>
2025-12-25 18:18:46 +0100

    Example login

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,47 @@
+use actix_identity::{Identity, IdentityMiddleware};
 use actix_session::{SessionMiddleware, config::PersistentSession, storage::CookieSessionStore};
-
 use actix_web::{
-    App, HttpResponse, HttpServer, Responder,
+    App, HttpMessage, HttpRequest, HttpResponse, HttpServer, Responder, Result,
     cookie::{Key, time::Duration},
-    get, post, web,
+    get,
+    http::StatusCode,
+    post, web,
 };
 
 const FIVE_MINUTES: Duration = Duration::minutes(5);
-
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index(user: Option<Identity>) -> Result<HttpResponse> {
+    if let Some(user) = user {
+        Ok(HttpResponse::build(StatusCode::OK)
+            .content_type("text/html; charset=utf-8")
+            .body(format!("Welcome! {}", user.id().unwrap())))
+    } else {
+        Ok(HttpResponse::build(StatusCode::OK).content_type("text/html; charset=utf-8").body(
+            "Welcome Anonymous!<br/>
+                <form method='POST' action='/login'>
+                <button type='submit'>log in</button>
+                </form>",
+        ))
+    }
+}
+#[post("/login")]
+async fn login(request: HttpRequest) -> impl Responder {
+    // Some kind of authentication should happen here
+    // e.g. password-based, biometric, etc.
+    // [...]
+
+    // attach a verified user identity to the active session
+    Identity::login(&request.extensions(), "User1".into()).unwrap();
+
+    HttpResponse::Ok()
+}
+
+#[post("/logout")]
+async fn logout(user: Option<Identity>) -> impl Responder {
+    if let Some(user) = user {
+        user.logout();
+    }
+    HttpResponse::Ok()
 }
 
 #[post("/echo")]
@@ -34,6 +65,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
+            .wrap(IdentityMiddleware::default())
             .wrap(
                 SessionMiddleware::builder(CookieSessionStore::default(), secret_key.clone())
                     .cookie_name("auth-example".to_owned())
@@ -41,7 +73,9 @@ async fn main() -> std::io::Result<()> {
                     .session_lifecycle(PersistentSession::default().session_ttl(FIVE_MINUTES))
                     .build(),
             )
-            .service(hello)
+            .service(index)
+            .service(login)
+            .service(logout)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
